29101580270fe7d5f0a2d3a6b74144b4f742e375
derbalkon <to.wrrrr@gmail.com>
2020-10-06 15:11:59 +0800

    add device choices

--- .config/yadm/setup.zsh
+++ .config/yadm/setup.zsh
@@ -1,23 +1,23 @@
 #!/bin/zsh
 set -e
-system_type=$(uname -s)
 cd $HOME
+system_type=$(uname -s)
 
 echo ">>> Preparing to restore environment..."
 
-# abort if it is not macOS
+# Abort if it is not macOS
 if [ "$system_type" != "Darwin" ]; then
   echo "Not supported: non-Apple OS."
   exit 1
 fi
 
-# install homebrew if it's missing
+# Install homebrew if it's missing
 if ! command -v brew >/dev/null 2>&1; then
   echo ">>> Installing homebrew.."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# set up once dotfiles repo has been cloned
+# Set up once dotfiles repo has been cloned
 echo ">>> Cloning the dotfiles repo and setting up..."
 brew install yadm
 yadm clone https://github.com/derbalkon/dotfiles.git --bootstrap
